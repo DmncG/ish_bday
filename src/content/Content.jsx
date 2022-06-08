852a130d33074f91deb7d13111170bb158cd218c
DmncG <micksdev@gmail.com>
2022-06-08 09:58:20 +0800

    updated message

--- src/content/Content.jsx
+++ src/content/Content.jsx
@@ -10,7 +10,7 @@ function Content () {
       <Header copy="Happy Birthday, Ish!"/>
       <img src={Pusheen} alt="pusheen" className="content__img" />
       <p className="content__p">
-        Sending you an actual postcard won't get there in time and it's not really possible right now so I made this instead.<br /><br />Happy happy birthday! I can't believe it hasn't been a month since we met each other in Hawaii, but it already feels like I've learned so much about you. I know the circumstances around us aren't ideal right now and there are going to be many challenges along the way but so far, I'm so happy to have met you and I hope that whatever we have continues to progress. :) <br /><br />Don't let your age stop you from having fun! Continue being your fun, sexy, and warm self. Wishing I could be there to celebrate with you.<br /><br />Missing you so much.
+        Sending you an actual postcard won't get there in time and it's not really possible right now so I made this instead.<br /><br />Happy happy birthday! I can't believe it has been a month since we met each other in Hawaii, but it already feels like I've learned so much about you. I know the circumstances around us aren't ideal right now and there are going to be many challenges along the way but so far, I'm so happy to have met you and I hope that whatever we have continues to progress. :) <br /><br />Don't let your age stop you from having fun! Continue being your fun, sexy, and warm self. Wishing I could be there to celebrate with you.<br /><br />Missing you so much.
 
         <br /><br />Dom  
       </p>
